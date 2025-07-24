62f2331df76f2041c5d2a9d9ec5c16743db7107c
Sérgio Duarte <sergiodaviduarte539@gmail.com>
2025-07-24 18:11:56 -0300

    Update Stack.py

--- Estrutura_Dados/Av1_Un3/Stack.py
+++ Estrutura_Dados/Av1_Un3/Stack.py
@@ -2,21 +2,24 @@ class Stack:
     def __init__(self):
         self.itens = []
 
-    def isEmpty(self):
+    def is_Empty(self):
         return self.itens == []
 
     def push(self, item):
         self.itens.append(item)
 
     def pop(self):
-        if not self.is_empty():
+        if not self.is_Empty():
             return self.itens.pop()
 
     def peek(self):
-        if not self.is_empty():
+        if not self.is_Empty():
             return self.itens[-1]
         else:
             return []
     
     def size(self):
         return len(self.itens)
+    
+    def __str__(self):
+        return str(self.itens)
