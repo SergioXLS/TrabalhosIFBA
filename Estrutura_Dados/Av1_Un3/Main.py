9ff784c2f649fecb44c8c6fb51ec7b8c3a356e95
Sérgio Duarte <sergiodaviduarte539@gmail.com>
2025-07-24 10:40:55 -0300

    Update Main.py

--- Estrutura_Dados/Av1_Un3/Main.py
+++ Estrutura_Dados/Av1_Un3/Main.py
@@ -1,1 +1,28 @@
+from Stack import *
 
+pagina_atual = None
+pagina_voltar = Stack()
+pagina_avancar = Stack()
+
+def exibir_estado():
+    print(f"\nPágina Atual: {pagina_atual}")
+    print(f"\nVoltar: {pagina_voltar}")
+    print(f"\nAvançar: {pagina_avancar}")
+
+def acessa_pagina(nova_pagina):
+    global pagina_atual
+    if pagina_atual is not None:
+        pagina_voltar.push(pagina_atual)
+        pagina_atual = nova_pagina
+
+def avancar():
+    global pagina_atual
+    if pagina_avancar.isEmpty():
+        pagina_voltar = pagina_atual
+        pagina_atual = pagina_avancar.pop()
+
+def voltar():
+    global pagina_atual
+    if not pagina_voltar.isEmpty():
+        pagina_avancar = pagina_atual
+        pagina_atual = pagina_voltar.pop()
