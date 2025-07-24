431a88cec87278b9faf486332ea678cd1d0ce158
Sérgio Duarte <sergiodaviduarte539@gmail.com>
2025-07-24 18:13:38 -0300

    Update FInal Main.py

--- Estrutura_Dados/Av1_Un3/Main.py
+++ Estrutura_Dados/Av1_Un3/Main.py
@@ -1,28 +1,63 @@
 from Stack import *
 
 pagina_atual = None
-pagina_voltar = Stack()
-pagina_avancar = Stack()
+pagina_antiga = Stack()
+proxima_pagina = Stack()
 
 def exibir_estado():
     print(f"\nPágina Atual: {pagina_atual}")
-    print(f"\nVoltar: {pagina_voltar}")
-    print(f"\nAvançar: {pagina_avancar}")
+    print(f"\nVoltar: {pagina_antiga}")
+    print(f"\nAvançar: {proxima_pagina}")
 
-def acessa_pagina(nova_pagina):
+def acessar_pagina(nova_pagina):
     global pagina_atual
     if pagina_atual is not None:
-        pagina_voltar.push(pagina_atual)
-        pagina_atual = nova_pagina
+        pagina_antiga.push(pagina_atual)
+    pagina_atual = nova_pagina
+    proxima_pagina = Stack()
+    return proxima_pagina
+    
 
 def avancar():
     global pagina_atual
-    if pagina_avancar.isEmpty():
-        pagina_voltar = pagina_atual
-        pagina_atual = pagina_avancar.pop()
+    if not proxima_pagina.is_Empty():
+        pagina_antiga.push(pagina_atual)
+        pagina_atual = proxima_pagina.pop()
+    else:
+        print("\n======================")
+        print("Pagina não encontrada")
+        print("======================")
 
 def voltar():
     global pagina_atual
-    if not pagina_voltar.isEmpty():
-        pagina_avancar = pagina_atual
-        pagina_atual = pagina_voltar.pop()
+    if not pagina_antiga.is_Empty():
+        proxima_pagina.push(pagina_atual)
+        pagina_atual = pagina_antiga.pop()
+    else:
+        print("\n======================")
+        print("Pagina não encontrada")
+        print("======================")
+        
+while True:
+    print("\n\t---NAVEGADOR---")
+    print("1 - Acessar nova página")
+    print("2 - Voltar")
+    print("3 - Avançar")
+    print("4 - Exibir pagina atual")
+    print("0 - Encerrar")
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == '1':
+        site = input("Digite o endereço da nova página: ")
+        acessar_pagina(site)
+    elif opcao == '2':
+        voltar()
+    elif opcao == '3':
+        avancar()
+    elif opcao == '4':
+        exibir_estado()
+    elif opcao == '0':
+        print("Encerrando...")
+        break
+    else:
+        print("Opção inválida!")
